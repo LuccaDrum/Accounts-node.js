ce002bb87de7557fbe2b32f70dc2063652cb599d
Lucca Drum <lucca.drum2004@gmail.com>
2026-07-30 20:11:09 -0300

    '1.0'

--- index.js
+++ index.js
@@ -21,10 +21,16 @@ function operation() {
         if (action === 'Criar conta') {
             createAccount()
             buildAccount()
+        } else if (action === 'Consultar saldo') {
+            getAccountBalance()
+        } else if (action === 'Depositar') {
+            deposit()
+        } else if (action === 'Sacar') {
+
+        } else if (action === 'Sair') {
+            console.log(chalk.bgBlue.black('Obrigado por usar o Accounts!'))
+            process.exit()
         }
-
-
-
 
     })
     .catch(err => {console.log(err)})
@@ -55,6 +61,7 @@ function buildAccount() {
         if (fs.existsSync(`accounts/${accountName}.json`)) {
             console.log(chalk.bgRed.black('Esta conta já existe, escolha outro nome!'))
             buildAccount()
+            return
         }
 
         fs.writeFileSync(`accounts/${accountName}.json`, '{"balance": 0}', function (err) {
@@ -66,4 +73,95 @@ function buildAccount() {
 
     })
     .catch(err => (console.log(err)))
+}
+
+function deposit() {
+
+    inquirer.prompt([
+        {
+            name: 'accountName',
+            message: 'Qual o nome da sua conta?'
+        }
+    ])
+    .then((answer) => {
+
+        const accountName = answer['accountName']
+
+        if (!checkAccount(accountName)) {
+            return deposit()
+        }
+
+        inquirer.prompt([
+            {
+            name: 'amount',
+            message: 'Quanto você deseja depositar?'
+        }
+        ])
+        .then((answer) => {
+
+            const amount = answer['amount']
+
+            addAmount(accountName, amount)
+            return operation()
+
+
+        })
+        .catch(err => (console.log(err)))
+
+    })
+    .catch(err => (console.log(err)))
+}
+
+function checkAccount(accountName) {
+
+    if (!fs.existsSync(`accounts/${accountName}.json`)) {
+        console.log(chalk.bgRed.black('Essa conta não existe, informe uma conta válida!'))
+        return false
+    } else {
+        return true
+    }
+}
+
+function addAmount(accountName, amount) {
+
+    const account = getAccount(accountName)
+
+    if (!amount) {
+        console.error(chalk.bgRed.black('Ocorreu um erro, tente novamente!'))
+        return deposit()
+    }
+
+    account.balance = parseFloat(amount) + parseFloat(account.balance)
+
+    fs.writeFileSync(`accounts/${accountName}.json`, JSON.stringify(account), (err) => {console.log(err)})
+
+    console.log(chalk.green(`Foi depositado o valor de R$${amount} na sua conta com sucesso!`))
+
+}
+
+function getAccount(accountName) {
+    const accountJSON = fs.readFileSync(`accounts/${accountName}.json`, {encoding: 'utf8', flag: 'r'})
+    return JSON.parse(accountJSON)
+
+}
+
+function getAccountBalance() {
+
+    inquirer.prompt([
+        {
+            name: 'accountName',
+            message: 'Qual o nome da sua conta?'
+        }
+    ])
+    .then((answer) => {
+    
+    const accountName = answer['accountName']
+
+    if (!checkAccount(accountName)) {
+        return getAccountBalance()
+    }
+        
+
+    })
+    .catch(err => console.log(err))
 }
